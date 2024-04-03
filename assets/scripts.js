d309e4c7af39595dd889e8ea1f0935cfdd4a9e3c
Ronan Berder <ronan@wiredcraft.com>
2024-04-03 13:56:17 +0800

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -9404,8 +9404,13 @@ $(function() {
   // Replace youtube links with an embed
   var width  = 640;
   var height = 480;
-  var embed = '<iframe width="'+ width +'" height="'+ height +'" src="https://www.youtube.com/embed/RIsiAluCxU4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
-
+  var embed = '<object width="'+ width +'" height="'+ height +'">'+
+            '  <param name="movie" value="http://www.youtube.com/v/[vid]&amp;hl=en&amp;fs=1"></param>'+
+            '  <param name="allowFullScreen" value="true"></param>'+
+            '  <param name="allowscriptaccess" value="always"></param>'+
+            '  <param name="wmode" value="transparent"></param>'+
+            '  <embed src="http://www.youtube.com/v/[vid]&amp;hl=en&amp;fs=1" type="application/x-shockwave-flash" wmode="transparent" allowscriptaccess="always" allowfullscreen="true" width="'+ width +'" '+'height="'+ height +'"></embed>'+
+            '</object>';
 
   $('.blog a[href*="youtube.com/watch"]').each(function() {
       var that = $(this);
@@ -9417,17 +9422,17 @@ $(function() {
       }
   });
 
-  $('a.preview[href*="youtube.com/watch"]').click(function() {
-      var that = $(this);
-      var video = that.attr('href').match(/(?:v=)([\w\-]+)/g);
+  // $('a.preview[href*="youtube.com/watch"]').click(function() {
+  //     var that = $(this);
+  //     var video = that.attr('href').match(/(?:v=)([\w\-]+)/g);
 
-      if (video.length) {
-          $.each(video, function(i){
-              $('#modal').html(embed.replace(/\[vid\]/g, this.replace('v=','')) );
-          });
-      }
-      $('body').addClass('modal');
+  //     if (video.length) {
+  //         $.each(video, function(i){
+  //             $('#modal').html(embed.replace(/\[vid\]/g, this.replace('v=','')) );
+  //         });
+  //     }
+  //     $('body').addClass('modal');
 
-      return false;
-  });
+  //     return false;
+  // });
 });
